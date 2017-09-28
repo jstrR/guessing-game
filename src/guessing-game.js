1a19811c59ff016a7cc717d8bb191533f504936c
jstrR <srovk22@gmail.com>
2017-09-28 18:00:44 +0300

    guessing-game 100%

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,21 @@
 class GuessingGame {
-    constructor() {}
+     constructor() {}
 
     setRange(min, max) {
-
+		this.max=max;
+		this.min=min;
     }
-
     guess() {
-
+		this.guessTry=Math.ceil((this.min + this.max) /2);
+		return this.guessTry;
     }
 
     lower() {
-
+		this.max=this.guessTry;
     }
 
     greater() {
-
+		this.min=this.guessTry;
     }
 }
-
 module.exports = GuessingGame;
